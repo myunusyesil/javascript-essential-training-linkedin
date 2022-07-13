e5be254408d55fcc2f7bbd090d1eb82f058a94ed
myunusyesil <mehmetyunus@windowslive.com>
2022-07-13 18:57:43 +0300

    created new element prepend it to existing html

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,15 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+let desc = document.querySelector(".site-description");
+desc.innerHTML = "deneme";
+
+let button = document.querySelectorAll(".lid-toggle");
+console.log(button);
+button.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    console.log(e.currentTarget);
+    e.currentTarget.style.backgroundColor = "green";
+  });
+});
--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,20 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+const header = document.querySelector(".siteheader");
+console.log(header);
+
+const content = `<div>
+<ul>
+  <li><a href="">Home</a></li>
+  <li><a href="">About</a></li>
+  <li><a href="">Services</a></li>
+  <li><a href="">Contact</a></li>
+</ul>
+</div>`;
+
+let navBar = document.createElement("nav");
+navBar.classList.add("nav-bar");
+navBar.innerHTML = content;
+header.prepend(navBar);
